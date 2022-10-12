8fc44f94784eed5ec1fa1489e4ac5c13622b939a
Amaliamelo <amalia.melo@aluno.ifsp.edu.br>
2022-10-12 20:31:14 -0300

    20221012

---
+++ exerciciosP1/exe07/main.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "ListaDinamica.h"
+
+
+int main() {    
+    TipoLista L;
+    TipoItem item;
+    TipoApontador p;
+
+    CriaLista(&L);
+
+	printf("Lista vazia? %d\n", ListaVazia(&L));
+	
+    item.chave = 0;
+    Insere(&L, item);
+
+	item.chave = 1;
+    Insere(&L, item);
+
+    item.chave = 1;
+    Insere(&L, item);
+	
+    item.chave = 0;
+    Insere(&L, item);
+
+	ImprimeLista(&L);
+	
+	Palindromo(&L);
+
+    p=L.primeiro;
+
+    while(p!=NULL){
+        L.primeiro = p->prox;
+        printf("Limpando\n");
+        free(p);
+        p=L.primeiro;
+    }
+
+	return 0;
+}
+
